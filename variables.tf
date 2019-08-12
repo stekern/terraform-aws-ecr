c25010b29e74e9a7e7bf26ca252d7e44074f487e
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-08-12 14:31:11 +0200

    Update to terraform 0.12.x

--- variables.tf
+++ variables.tf
@@ -4,20 +4,23 @@
 
 variable "name_prefix" {
   description = "A prefix used for naming resources."
+  type        = string
 }
 
 variable "max_images_retained" {
   description = "The max number of images to keep in the repository before expiring the oldest"
+  type        = number
   default     = 100
 }
 
 variable "tags" {
   description = "A map of tags (key-value pairs) passed to resources."
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "trusted_accounts" {
   description = "IDs of other accounts that are trusted to pull and push to this repostitory"
-  type        = "list"
+  type        = list(string)
 }
+
